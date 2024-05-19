a3b7872f97a8f927ac1330a56e6e565c1f580a44
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2024-05-19 11:53:17 +0100

    Update AuthInputs.jsx

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,24 +1,8 @@
 import { useState } from 'react';
-import {styled} from 'styled-components'
 
 import Button from './Button.jsx'
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-  const SecondButton = styled.button`
-  & text-button {
-    color: #f0b322;
-    border: none;
-  }
-  & text-button:hover {
-    color: #f0920e;
-  }
-  `
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -40,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className ="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800 ">
+      <div className="flex flex-col gap-2 mb-6"> 
           <Input
           label='Email'
             invalid={emailNotValid}
@@ -57,11 +41,11 @@ export default function AuthInputs() {
             onChange={(event) =>
             handleInputChange('password', event.target.value)}
           />
-      </ControlContainer>
-      <div className="actions">
-        <SecondButton type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
-        </SecondButton>
+        </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
